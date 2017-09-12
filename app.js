b9745e1ab916a9dc80461c48eef142ecf40569b6
toneloke <guerrerotonyjs@gmail.com>
2017-09-12 17:03:06 -0400

    todo form done fineto

--- app.js
+++ app.js
@@ -11,7 +11,15 @@ class ToDoForm {
     // myDiv.className = 'form-container'
     // document.body.append(myDiv)
     // myDiv.innerHTML = ToDoForm.renderForm()
-    document.getElementById(divID).innerHTML = ToDoForm.renderForm()
+    document.getElementById(divID).innerHTML = this.renderForm()
+    document.querySelector('form').addEventListener('submit', ToDoForm.onSubmit)
+  }
+  static onSubmit () {
+    event.preventDefault()
+    let formText = event.target[0].value
+    event.target[0].value = ''
+    let newTodoItem = new TodoItem(formText)
+    newTodoItem.appendToList()
   }
   static renderForm () {
     return `<div class="row">
@@ -23,14 +31,58 @@ class ToDoForm {
                     <label for="icon_prefix">Todo Item</label>
                   </div>
                   <div class="input-field col s6">
-                     <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+                     <button type="submit" class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></button>
                   </div>
                 </div>
               </form>
             </div>`
   }
 }
-ToDoForm.appendToDiv('form-container')
+ToDoForm.appendToDiv('form-container') // the form becomes reality
 // let myform = ToDoForm.constructor()
 // let myForm = new ToDoForm()
 // myForm.appendToDiv() -> moved to constructor
+
+class TodoItem {
+  constructor (textInput) {
+    this.itemDiv = document.getElementById('item-container')
+    this.text = textInput
+    this.done = false
+    this.itemDiv.addEventListener('click', this.complete)
+  }
+  complete () {
+    // console.log('todoItem-clicked = ', event.target.id)
+    let clickedEl = event.target.id
+    switch (clickedEl) {
+      case 'done':
+        event.target.parentElement.parentElement.className = 'done'
+        break
+      default:
+
+    }
+  }
+  renderItem () {
+    return `<li id="${this.text}" class="collection-item">
+                <div>${this.text}
+                  <a href="#!" class="secondary-content">
+                    <i id="done" class="material-icons">check</i>
+                  </a>
+                </div>
+            </li>`
+  }
+  appendToList () {
+    this.itemDiv.innerHTML += this.renderItem()
+  }
+}
+
+// let color = 'white'
+// switch (name) {
+//   case 'purple':
+//       console.log('tony')
+//     break;
+//   case 'pink':
+//       console.log('waldo')
+//     break;
+//   default:
+//
+// }
